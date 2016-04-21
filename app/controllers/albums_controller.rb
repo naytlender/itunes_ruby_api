21264de59a4fc2c63b2fcf3da0ebeebf37c9388e
naytlender <naytlender@gmail.com>
2016-04-21 11:28:53 +0300

    fix album controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -5,10 +5,6 @@ require 'json'
 # albums controller
 class AlbumsController
   def self.index(itunes_id)
-<<<<<<< HEAD
     @albums = Artist.find_by(itunes_id: itunes_id).albums
-=======
-    @albums = Artist.find_by_id(itunes_id).albums
->>>>>>> 4f647deaa0190d21effb777bd4ab79305fb48d8c
   end
 end
